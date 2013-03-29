6a8d7b6fea0996a0cb716f0c5bdc63027d7a983f
Gniewomir Świechowski <gniewomir.swiechowski@gmail.com>
2013-03-29 18:36:23 +0100

    Revert "test #14"
    This reverts commit e661ca304de50a47914e19b16453e1e6b1f41d25.

--- js/report.js
+++ js/report.js
@@ -11,6 +11,8 @@ $('#reportsPage').on('pageshow', function(event){
   	},
 
 	displayReports: function(){
+		//$('#reportsSelect-button .ui-btn-text').append('<div class="btnloader"></div>'); 
+		//$('#reportsSelect-button .ui-btn-text .btnloader').css('display','inline-block');			
 		var auth_key = localStorage.getItem("auth_key");
 		if($('#reportsSelect').find('option').length == 1){
 			$.ajax({
@@ -22,34 +24,37 @@ $('#reportsPage').on('pageshow', function(event){
 				crossDomain: true,
 				contentType: 'application/json; charset=utf-8',
 				success: function(data){  
+					//$('#reportsSelect-button .ui-btn-text .btnloader').css('display','none');					
 					$('#reportsSelect').html('<option data-placeholder="true" value="placeholder">Wybierz</option>');
-					$('#reportsSelect').append('<option value="34534534">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc a purus mauris. Ut gravida tempor metus ultricies scelerisque. Aenean nec diam lorem, et interdum augue. Cras sit amet posuere tortor.</option>');
-					$('#reportsSelect').append('<option value="34534534">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc a purus mauris. Ut gravida tempor metus ultricies scelerisque. Aenean nec diam lorem, et interdum augue. Cras sit amet posuere tortor.</option>');
-					$('#reportsSelect').append('<option value="34534534">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc a purus mauris. Ut gravida tempor metus ultricies scelerisque. Aenean nec diam lorem, et interdum augue. Cras sit amet posuere tortor.</option>');
-					$('#reportsSelect').append('<option value="34534534">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc a purus mauris. Ut gravida tempor metus ultricies scelerisque. Aenean nec diam lorem, et interdum augue. Cras sit amet posuere tortor.</option>');
-					$('#reportsSelect').append('<option value="34534534">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc a purus mauris. Ut gravida tempor metus ultricies scelerisque. Aenean nec diam lorem, et interdum augue. Cras sit amet posuere tortor.</option>');
-					$('#reportsSelect').append('<option value="34534534">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc a purus mauris. Ut gravida tempor metus ultricies scelerisque. Aenean nec diam lorem, et interdum augue. Cras sit amet posuere tortor.</option>');
-					$('#reportsSelect').append('<option value="34534534">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc a purus mauris. Ut gravida tempor metus ultricies scelerisque. Aenean nec diam lorem, et interdum augue. Cras sit amet posuere tortor.</option>');
-					$('#reportsSelect').append('<option value="34534534">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc a purus mauris. Ut gravida tempor metus ultricies scelerisque. Aenean nec diam lorem, et interdum augue. Cras sit amet posuere tortor.</option>');
-					$('#reportsSelect').append('<option value="34534534">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc a purus mauris. Ut gravida tempor metus ultricies scelerisque. Aenean nec diam lorem, et interdum augue. Cras sit amet posuere tortor.</option>');
-					$('#reportsSelect').append('<option value="34534534">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc a purus mauris. Ut gravida tempor metus ultricies scelerisque. Aenean nec diam lorem, et interdum augue. Cras sit amet posuere tortor.</option>');
-					$('#reportsSelect').append('<option value="34534534">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc a purus mauris. Ut gravida tempor metus ultricies scelerisque. Aenean nec diam lorem, et interdum augue. Cras sit amet posuere tortor.</option>');
-					$('#reportsSelect').append('<option value="34534534">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc a purus mauris. Ut gravida tempor metus ultricies scelerisque. Aenean nec diam lorem, et interdum augue. Cras sit amet posuere tortor.</option>');
-					$('#reportsSelect').append('<option value="34534534">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc a purus mauris. Ut gravida tempor metus ultricies scelerisque. Aenean nec diam lorem, et interdum augue. Cras sit amet posuere tortor.</option>');
-					$('#reportsSelect').append('<option value="34534534">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc a purus mauris. Ut gravida tempor metus ultricies scelerisque. Aenean nec diam lorem, et interdum augue. Cras sit amet posuere tortor.</option>');
-					$('#reportsSelect').append('<option value="34534534">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc a purus mauris. Ut gravida tempor metus ultricies scelerisque. Aenean nec diam lorem, et interdum augue. Cras sit amet posuere tortor.</option>');
-					$('#reportsSelect').append('<option value="34534534">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc a purus mauris. Ut gravida tempor metus ultricies scelerisque. Aenean nec diam lorem, et interdum augue. Cras sit amet posuere tortor.</option>');
-					$('#reportsSelect').append('<option value="34534534">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc a purus mauris. Ut gravida tempor metus ultricies scelerisque. Aenean nec diam lorem, et interdum augue. Cras sit amet posuere tortor.</option>');
-					
+/*
+					$('#reportsSelect').append('<option  >Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc a purus mauris. Ut gravida tempor metus ultricies scelerisque. Aenean nec diam lorem, et interdum augue. Cras sit amet posuere tortor.</option>');
+					$('#reportsSelect').append('<option  >Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc a purus mauris. Ut gravida tempor metus ultricies scelerisque. Aenean nec diam lorem, et interdum augue. Cras sit amet posuere tortor.</option>');
+					$('#reportsSelect').append('<option  >Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc a purus mauris. Ut gravida tempor metus ultricies scelerisque. Aenean nec diam lorem, et interdum augue. Cras sit amet posuere tortor.</option>');
+					$('#reportsSelect').append('<option  >Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc a purus mauris. Ut gravida tempor metus ultricies scelerisque. Aenean nec diam lorem, et interdum augue. Cras sit amet posuere tortor.</option>');
+					$('#reportsSelect').append('<option  >Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc a purus mauris. Ut gravida tempor metus ultricies scelerisque. Aenean nec diam lorem, et interdum augue. Cras sit amet posuere tortor.</option>');
+					$('#reportsSelect').append('<option  >Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc a purus mauris. Ut gravida tempor metus ultricies scelerisque. Aenean nec diam lorem, et interdum augue. Cras sit amet posuere tortor.</option>');
+					$('#reportsSelect').append('<option  >Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc a purus mauris. Ut gravida tempor metus ultricies scelerisque. Aenean nec diam lorem, et interdum augue. Cras sit amet posuere tortor.</option>');
+					$('#reportsSelect').append('<option  >Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc a purus mauris. Ut gravida tempor metus ultricies scelerisque. Aenean nec diam lorem, et interdum augue. Cras sit amet posuere tortor.</option>');
+					$('#reportsSelect').append('<option  >Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc a purus mauris. Ut gravida tempor metus ultricies scelerisque. Aenean nec diam lorem, et interdum augue. Cras sit amet posuere tortor.</option>');
+					$('#reportsSelect').append('<option  >Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc a purus mauris. Ut gravida tempor metus ultricies scelerisque. Aenean nec diam lorem, et interdum augue. Cras sit amet posuere tortor.</option>');
+					$('#reportsSelect').append('<option  >Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc a purus mauris. Ut gravida tempor metus ultricies scelerisque. Aenean nec diam lorem, et interdum augue. Cras sit amet posuere tortor.</option>');
+					$('#reportsSelect').append('<option  >Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc a purus mauris. Ut gravida tempor metus ultricies scelerisque. Aenean nec diam lorem, et interdum augue. Cras sit amet posuere tortor.</option>');
+					$('#reportsSelect').append('<option  >Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc a purus mauris. Ut gravida tempor metus ultricies scelerisque. Aenean nec diam lorem, et interdum augue. Cras sit amet posuere tortor.</option>');
+					$('#reportsSelect').append('<option  >Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc a purus mauris. Ut gravida tempor metus ultricies scelerisque. Aenean nec diam lorem, et interdum augue. Cras sit amet posuere tortor.</option>');
+					$('#reportsSelect').append('<option  >Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc a purus mauris. Ut gravida tempor metus ultricies scelerisque. Aenean nec diam lorem, et interdum augue. Cras sit amet posuere tortor.</option>');
+					$('#reportsSelect').append('<option  >Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc a purus mauris. Ut gravida tempor metus ultricies scelerisque. Aenean nec diam lorem, et interdum augue. Cras sit amet posuere tortor.</option>');
+					$('#reportsSelect').append('<option  >Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc a purus mauris. Ut gravida tempor metus ultricies scelerisque. Aenean nec diam lorem, et interdum augue. Cras sit amet posuere tortor.</option>');
+*/
 					$.each(data.raporty, function(i, item){
-					//	$('#reportsSelect').append('<option value="' + item.raport_kod + '">' + item.raport_nazwa + '</option>');
-					})
-					
+						$('#reportsSelect').append('<option value="' + item.raport_kod + '">' + item.raport_nazwa + '</option>');
+					});
 					$('#reportsSelect').selectmenu('refresh');
+					//$('#reportsSelect').selectmenu('enable');
 					$('#createReport').attr('href','');
 					$('#createReport span span').html('Wygeneruj raport (pdf) <div class="btnloader"></div>');
 					$('#createReport').trigger('create');
-					
+					//$('#reportsPage .loadingmsg').hide();
+					//$('#reportsPage .ui-content > * ').show();
 	       		},
 				error: function(message){
 
