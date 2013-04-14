e95aaf017c6bd03773f1fc8e1ca069440656a2af
Pawel <ufo.faflik@gmail.com>
2013-04-14 20:49:44 +0200

    Test otwierania w nowym oknie 2

--- js/application.js
+++ js/application.js
@@ -116,5 +116,5 @@ $('.openlink').on('tap', function() {
 
 $('#wwwNotification').on('tap', function() {
     alert('Przejdź do przeglądarki by odwiedzić stronę');
-    window.open( $(this).attr('url'), "_system");
+    window.open( $(this).attr('url'), '_blank' ,'location=yes');
 });
